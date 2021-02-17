5f22ce675129bd0269c7995395d8fc714e9a7132
Shobhna Singhal <agarwalshobhna61@gmail.com>
2021-02-17 23:30:22 +0000

    This is my week-4 commit

---
+++ Week-4/script4.js
@@ -0,0 +1,7 @@
+function myLoadFunction() {
+var element = document.getElementById('pageheading');
+element.firstChild.nodeValue = 'New Heading';
+var element = document.getElementById('pageparagraph');
+element.firstChild.nodeValue = 'New paragraph and lots of useful information about module.';
+}
+document.addEventListener('DOMContentLoaded', myLoadFunction);
